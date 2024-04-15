99d8633c83ac901f24037833620075164b21cae9
arnavgupta00 <arnavguptagg@gmail.com>
2024-04-16 00:10:35 +0530

    adding coinbase 5

--- src/app/api/coinbase/webhook/route.ts
+++ src/app/api/coinbase/webhook/route.ts
@@ -2,26 +2,16 @@ import { Webhook } from "coinbase-commerce-node";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: any) {
-  const data = await req.body;
-  const body = await req.json();
-
   const secret = process.env.COINBASE_SECRET || "";
-
-  console.log("Webhook body:", body);
-  console.log("Webhook data:", data);
 
   const nextReq = new NextRequest(req);
   const rawBody = await nextReq.text();
   const webhookSignature = nextReq.headers.get("x-cc-webhook-signature") || "";
 
-console.log("Webhook signature:",rawBody, webhookSignature);
+  console.log("Webhook signature:", rawBody, webhookSignature);
 
   try {
-    const event = Webhook.verifyEventBody(
-      rawBody,
-      webhookSignature,
-      secret
-    );
+    const event = Webhook.verifyEventBody(rawBody, webhookSignature, secret);
 
     console.log("Webhook event:", event);
 
